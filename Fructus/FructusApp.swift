eba2aa478add6373ced7b693f1175bd72f31c027
Duong, Viet Anh <dva.github@gmail.com>
2024-03-22 15:23:50 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by why_me on 22.03.24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
